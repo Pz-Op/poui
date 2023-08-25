14ae215b3cf195abe544a618736f9c3785043087
PzHown <1263097055@qq.com>
2023-08-25 16:59:39 +0800

    style(index.html、app.vue ...): styles changes

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,60 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header class="bg-[var(--md-sys-color-background)]">
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125">
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <div class="mt-4 flex flex-wrap gap-4">
-        <Button potype="primary">
-          1255
-        </Button>
-        <Button disabled>
-          1255
-        </Button>
-        <Button>1255</Button>
-        <Button potype="text">
-          1255
-        </Button>
-        <Button potype="outline">
-          1255
-        </Button>
-      </div>
-      <div class="mt-4 flex flex-wrap gap-4">
-        <Button size="sm" potype="primary">
-          1255
-        </Button>
-        <Button size="sm" disabled>
-          1255
-        </Button>
-        <Button size="sm">
-          1255
-        </Button>
-        <Button size="sm" potype="text">
-          1255
-        </Button>
-        <Button size="sm" potype="outline">
-          1255
-        </Button>
-      </div>
-      <div class="mt-4 flex flex-wrap gap-4">
-        <Button size="lg" potype="primary">
-          1255
-        </Button>
-        <Button size="lg" disabled>
-          1255
-        </Button>
-        <Button size="lg">
-          1255
-        </Button>
-        <Button size="lg" potype="text">
-          1255
-        </Button>
-        <Button size="lg" potype="outline">
-          1255
-        </Button>
-      </div>
+  <div class="wrapper bg-[var(--md-sys-color-background)] p-4">
+    <div class="mt-4 flex flex-wrap gap-4">
+      <Button potype="primary">
+        1255
+      </Button>
+      <Button disabled>
+        1255
+      </Button>
+      <Button>1255</Button>
+      <Button potype="text">
+        1255
+      </Button>
+      <Button potype="outline">
+        1255
+      </Button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="mt-4 flex flex-wrap gap-4">
+      <Button size="sm" potype="primary">
+        1255
+      </Button>
+      <Button size="sm" disabled>
+        1255
+      </Button>
+      <Button size="sm">
+        1255
+      </Button>
+      <Button size="sm" potype="text">
+        1255
+      </Button>
+      <Button size="sm" potype="outline">
+        1255
+      </Button>
+    </div>
+    <div class="mt-4 flex flex-wrap gap-4">
+      <Button size="lg" potype="primary">
+        1255
+      </Button>
+      <Button size="lg" disabled>
+        1255
+      </Button>
+      <Button size="lg">
+        1255
+      </Button>
+      <Button size="lg" potype="text">
+        1255
+      </Button>
+      <Button size="lg" potype="outline">
+        1255
+      </Button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
